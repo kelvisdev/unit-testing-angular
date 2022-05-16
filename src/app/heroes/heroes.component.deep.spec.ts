efcda547a84417eb3e31c8097bb6d1a2ac75831b
Kelvis Rodrigues Borges <kelvisbcc@gmail.com>
2022-05-16 11:02:00 -0300

    add unit test deep to heroesComponent

---
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -0,0 +1,53 @@
+import { forEach } from "@angular-devkit/schematics";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { of } from "rxjs";
+import { HeroService } from "../hero.service";
+import { HeroComponent } from "../hero/hero.component";
+import { HeroesComponent } from "./heroes.component";
+
+fdescribe("HeroesComponent (deep tests)", () => {
+  let fixture: ComponentFixture<HeroesComponent>;
+  let mockHeroService;
+  let HEROES;
+
+  beforeEach(() => {
+    mockHeroService = jasmine.createSpyObj([
+      "getHeroes",
+      "addHero",
+      "deleteHero",
+    ]);
+
+    HEROES = [
+      { id: 1, name: "SpiderDude", strength: 8 },
+      { id: 2, name: "Wonderful Woman", strength: 24 },
+      { id: 3, name: "SuperDude", strength: 55 },
+    ];
+
+    TestBed.configureTestingModule({
+      declarations: [HeroesComponent, HeroComponent],
+      providers: [{ provide: HeroService, useValue: mockHeroService }],
+    });
+    fixture = TestBed.createComponent(HeroesComponent);
+
+    fixture = TestBed.createComponent(HeroesComponent);
+  });
+
+  it("should render each hero as a HeroComponent", () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+    // run ngOnInit
+    fixture.detectChanges();
+
+    const heroComponentDes = fixture.debugElement.queryAll(By.directive(HeroComponent));
+    expect(heroComponentDes.length).toEqual(3);
+
+    heroComponentDes.forEach((heroComponent, index) => {
+      expect(heroComponent.componentInstance.hero).toEqual(HEROES[index]);
+    });
+
+    // expect(heroComponentDes[0].componentInstance.hero.name).toEqual('SpiderDude');
+    // expect(heroComponentDes[1].componentInstance.hero.name).toEqual('Wonderful Woman');
+    // expect(heroComponentDes[2].componentInstance.hero.name).toEqual('SuperDude');
+  });
+});
